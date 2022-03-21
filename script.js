699e2d5d06683a1ceb76e78d87a39c6d23506aa0
sony <karthikeyan.a@live.com>
2022-03-21 13:17:12 +0530

    assessments done

---
+++ script.js
@@ -0,0 +1,44 @@
+// 
+
+// assignment 2
+function myFunction(x) {
+    x.classList.toggle('change');
+  }
+  
+// assignment 3
+var acc = document.getElementsByClassName('accordion');
+var i;
+
+    for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener('click', function () {
+        this.classList.toggle('active');
+        var panel = this.nextElementSibling;
+        if (panel.style.display === 'block') {
+            panel.style.display = 'none';
+        } else {
+            panel.style.display = 'block';
+        }
+    });
+}
+
+// assignment 4
+
+function openCity(evt, cityName) {
+    var i, tabcontent, tablinks;
+  
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+  
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+  
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+// assignment 5
+
